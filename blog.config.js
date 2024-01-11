efb0ac931420e79ad1c5237893b205fc768f0ab1
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2024-01-11 02:39:32 -0600

    retry

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   craftConfigShareUrl: process.env.CRAFT_CONFIG_SHARE_URL
     ? process.env.CRAFT_CONFIG_SHARE_URL
-    : 'https://docs.craft.do/editor/d/7949ae5d-3740-e0d6-4b90-ff236f81a24b/d3221ac2-3c7b-37a0-fa6a-29137337ee65/', // The link to share your craft config
+    : 'https://www.craft.do/s/HWxY0rUllA2GQL', // The link to share your craft config
   seo: {
     keywords: ['Blog', 'Craft.do', 'Craft Docs', 'Next.js', 'TailwindCSS']
   },
